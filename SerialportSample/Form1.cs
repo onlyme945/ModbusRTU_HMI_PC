37d60afc420568c32bef1ea9fa1f280fc683fbf9
Truman <onlyme945laptopdell@hotmail.com>
2018-03-01 19:42:12 +0800

    子1进3

--- SerialportSample/Form1.cs
+++ SerialportSample/Form1.cs
@@ -16,6 +16,7 @@ namespace SerialportSample
         /*在master_child1中能看到*/
         /*子1进1*/
         /*子1进2*/
+        /*子1进3*/
         private ModbusRTU ModbusMaster = new ModbusRTU();
         private SerialPort comm = new SerialPort();
         private StringBuilder builder = new StringBuilder();//避免在事件处理方法中反复的创建，定义到外面。
