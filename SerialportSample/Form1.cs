da157397306da5fe1594831dba7229d824e04902
OnlyMe <OnlyMe@DESKTOP-7P4R0MN>
2018-03-01 19:56:35 +0800

    子进5

--- SerialportSample/Form1.cs
+++ SerialportSample/Form1.cs
@@ -18,6 +18,8 @@ namespace SerialportSample
         /*子1进2*/
         /*子1进3*/
         /*子1进4*/
+        /*子1进5*/
+       
         private ModbusRTU ModbusMaster = new ModbusRTU();
         private SerialPort comm = new SerialPort();
         private StringBuilder builder = new StringBuilder();//避免在事件处理方法中反复的创建，定义到外面。
