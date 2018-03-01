ce94afa933bf0164854667524b5c67ee3b184175
OnlyMe <OnlyMe@DESKTOP-7P4R0MN>
2018-03-01 19:11:58 +0800

    子1进1

--- SerialportSample/Form1.cs
+++ SerialportSample/Form1.cs
@@ -14,6 +14,7 @@ namespace SerialportSample
     public partial class SerialportSampleForm : Form
     {
         /*在master_child1中能看到*/
+        /*子1进1*/
         private ModbusRTU ModbusMaster = new ModbusRTU();
         private SerialPort comm = new SerialPort();
         private StringBuilder builder = new StringBuilder();//避免在事件处理方法中反复的创建，定义到外面。
